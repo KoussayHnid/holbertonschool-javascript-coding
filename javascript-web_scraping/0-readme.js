3565eb386178f353616d60e2ba3ebe6d678a081b
koussay557 <henidkousay@gmail.com>
2023-09-22 14:03:01 +0100

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const arg = process.argv[2];
 const fs = require('fs');
 
 fs.readFile(arg, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
