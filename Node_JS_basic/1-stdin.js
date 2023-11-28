2b5ebc96e5898b5f200892bbfc1364015437f3e4
koussay557 <henidkousay@gmail.com>
2023-11-28 13:27:32 +0100

    script that that will be executed through command line

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,15 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdin.resume();
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-console.log("Welcome to Holberton School, what is your name?");
-
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  console.log("This important software is now closing");
-  rl.close();
-});
-
-rl.on('close', () => {
-  process.exit(0);
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
