8deb05b3599f1f1bd0ae91dd7da19beeb2a4be43
koussay557 <henidkousay@gmail.com>
2023-11-30 10:29:18 +0100

    task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,26 +1,25 @@
 const express = require('express');
-const students = require('./3-read_file_async');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
-const port = 1245;
+const PORT = 1245;
+const databasePath = process.argv[2];
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  students(process.argv[2])
-    .then((data) => {
-      const response = `This is the list of our students\n${data}`;
-      res.send(response);
-    })
-    .catch((error) => {
-      res.status(500).send(`This is the list of our students\nCannot load the database\n${error.message}`);
-    });
+app.get('/students', async (req, res) => {
+  try {
+    const studentsData = await countStudents(databasePath);
+    res.send(`This is the list of our students\n${studentsData}`);
+  } catch (error) {
+    res.status(500).send(`Internal Server Error: ${error.message}`);
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running and listening on port ${PORT}`);
 });
 
 module.exports = app;
