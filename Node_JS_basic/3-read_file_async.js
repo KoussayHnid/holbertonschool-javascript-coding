0205975eac7e9e3a38ac5f33769b3c458afff277
koussay557 <henidkousay@gmail.com>
2023-11-30 10:30:09 +0100

    task 3

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -7,21 +7,21 @@ function countStudents(path) {
         reject(new Error('Cannot load the database'));
       } else {
         const lines = data.split('\n').filter((line) => line.trim() !== '');
-        const counters = {};
+        const fieldCounts = {};
 
         for (let i = 1; i < lines.length; i += 1) {
           const [firstName, , , field] = lines[i].split(',');
           if (field) {
-            counters[field] = counters[field] || { count: 0, names: [] };
-            counters[field].count += 1;
-            counters[field].names.push(firstName.trim());
+            fieldCounts[field] = fieldCounts[field] || { count: 0, names: [] };
+            fieldCounts[field].count += 1;
+            fieldCounts[field].names.push(firstName.trim());
           }
         }
 
         console.log(`Number of students: ${lines.length - 1}`);
-        for (const field in counters) {
-          if (Object.prototype.hasOwnProperty.call(counters, field)) {
-            console.log(`Number of students in ${field}: ${counters[field].count}. List: ${counters[field].names.join(', ')}`);
+        for (const field in fieldCounts) {
+          if (Object.prototype.hasOwnProperty.call(fieldCounts, field)) {
+            console.log(`Number of students in ${field}: ${fieldCounts[field].count}. List: ${fieldCounts[field].names.join(', ')}`);
           }
         }
         resolve();
